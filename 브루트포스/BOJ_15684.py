19a42816734f2b656b40a6393c83dd8f71dcb645
nuuuri <sbfl125@gmail.com>
2022-07-23 06:27:17 +0900

    BOJ #15684 사다리 조작

--- 브루트포스/BOJ_15684.py
+++ 브루트포스/BOJ_15684.py
@@ -1,46 +1,48 @@
-n, m, h = map(int, input().split())
-arr = [[0 for _ in range(n)] for _ in range(h)]
-ans = -1
+# G[3] 메모리 119256 KB  시간 3796 ms
 
-for _ in range(m):
+N, M, H = map(int, input().split())
+arr = [[0] * N for _ in range(H)]
+ans = 4
+
+for _ in range(M):
     a, b = map(int, input().split())
     arr[a-1][b-1] = 1
     arr[a-1][b] = -1
 
-def result(_arr, idx):
-    res = idx
-    for i in range(h):
-        if _arr[i][res] == 0:
-            continue
-        else:
-            res += _arr[i][res]
-    return res
+def check():
+    for n in range(N):
+        idx = n
 
-def check(_arr):
-    res = []
-    for i in range(n):
-        res.append(result(_arr, i))
-    return res == [x for x in range(n)]
+        for h in range(H):
+            idx += arr[h][idx]
+        
+        if idx != n:
+            return False
+    
+    return True
 
-def addLine(_arr, idx, cnt):
+
+def dfs(x, y, cnt):
     global ans
 
-    if cnt == 4 or idx == h:
+    if cnt == 4:
         return
 
-    if check(_arr):
-        if ans == -1 or ans > cnt:
-            ans = cnt
-        return 
+    if check() and ans > cnt:
+        ans = cnt
+        return
 
-    addLine(_arr, idx+1, cnt)
+    for i in range(x, H):
+        k = y if i == x else 0
+        for j in range(k, N-1):
+            if arr[i][j] == 0 and arr[i][j+1] == 0:
+                arr[i][j] = 1
+                arr[i][j+1] = -1
 
-    for i in range(n-1):
-        new_arr = [item[:] for item in _arr]
-        if new_arr[idx][i] == 0 and new_arr[idx][i+1] == 0:
-            new_arr[idx][i] = 1
-            new_arr[idx][i+1] = -1
-            addLine(new_arr, idx+1, cnt+1)
+                dfs(i, j+2, cnt+1)
 
-addLine(arr, 0, 0)
-print(ans)
+                arr[i][j] = 0
+                arr[i][j+1] = 0
+
+dfs(0, 0, 0)
+print(ans if ans < 4 else -1)
