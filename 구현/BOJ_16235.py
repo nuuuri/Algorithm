27e24b0270201dddda9920ac0abfdca483e14299
nuuuri <sbfl125@gmail.com>
2022-07-29 12:48:56 +0900

    BOJ #16235 나무 재테크

--- 구현/BOJ_16235.py
+++ 구현/BOJ_16235.py
@@ -1,4 +1,4 @@
-# G[3]
+# G[3] 메모리 152660 KB  시간 1152 ms
 
 N, M, K = map(int, input().split())
 A = [[x for x in map(int, input().split())] for _ in range(N)]
@@ -10,33 +10,41 @@ for _ in range(M):
     x, y, age = map(int, input().split())
     trees[x-1][y-1].append(age)
 
+trees = [[sorted(trees[i][j]) for j in range(N)] for i in range(N)]
+
 for _ in range(K):
-    # 1. 봄 & 여름
+    # 봄 & 여름
     for i in range(N):
         for j in range(N):
-            trees[i][j].sort()
-            cnt = 0
             temp = trees[i][j][:]
-            
-            for k, age in enumerate(trees[i][j][:]):
+            new_trees = []
+
+            while temp:
+                age = temp[0]
                 if age <= arr[i][j]:
                     arr[i][j] -= age
-                    trees[i][j][k - cnt] += 1
+                    new_trees.append(age + 1)
+                    temp.pop(0)
                 else:
-                    arr[i][j] += int(age / 2)
-                    trees[i][j].pop(k - cnt)
-                    cnt += 1
+                    break
+            
+            while temp:
+                age = temp.pop(0)
+                arr[i][j] += int(age / 2)
 
-    # 3. 가을
+            trees[i][j] = new_trees
+
+    # 가을
+    temp = [item[:] for item in trees]
     for x in range(N):
         for y in range(N):
-            for tree in trees[x][y]:
+            for tree in temp[x][y]:
                 if tree % 5 == 0:
                     for dx, dy in [[-1,-1],[-1,0],[-1,+1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]:
                         if 0 <= x+dx < N and 0 <= y+dy < N:
                             trees[x+dx][y+dy].insert(0, 1)
 
-    # 4. 겨울
+    # 겨울
     for i in range(N):
         for j in range(N):
             arr[i][j] += A[i][j]
