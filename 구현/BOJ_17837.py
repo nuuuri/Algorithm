9cc38c28780faaa0cacfb777ee0fc5605451c549
nuuuri <sbfl125@gmail.com>
2022-08-05 17:26:08 +0900

    BOJ #17837 새로운 게임 2

--- 구현/BOJ_17837.py
+++ 구현/BOJ_17837.py
@@ -1,10 +1,9 @@
-# G[2] 메모리 117900 KB  시간 276 ms
+# G[2] 메모리 117208 KB  시간 256 ms
 
 N, K = map(int, input().split())
 MAP = [list(map(int, input().split())) for _ in range(N)]
 DIR = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 board = [[[] for _ in range(N)] for _ in range(N)]
-cnt = 0
 
 def turn(d):
     if d % 2 == 0:
@@ -28,36 +27,32 @@ def move(i, j, k):
         chessman[0] = (chessman[0][0], turn(chessman[0][1]))
         di, dj = DIR[chessman[0][1]]
 
-    if 0 <= i+di < N and 0 <= j+dj < N:
-        if MAP[i+di][j+dj] == 0:
-            board[i+di][j+dj].extend(chessman)
-        elif  MAP[i+di][j+dj] == 1:
-            board[i+di][j+dj].extend(chessman[::-1])
-        else:
-            board[i][j].extend(chessman)
-    else:
+    if i+di < 0 or i+di >= N or j+dj < 0 or j+dj >= N or MAP[i+di][j+dj] == 2:
         board[i][j].extend(chessman)
-
-    if 0 <= i+di < N and 0 <= j+dj < N:
-        return len(board[i+di][j+dj])
-    else:
         return 0
+    elif MAP[i+di][j+dj] == 0:
+        board[i+di][j+dj].extend(chessman)
+    else:
+        board[i+di][j+dj].extend(chessman[::-1])
+
+    return len(board[i+di][j+dj])
 
 for i in range(K):
     x, y, d = map(int, input().split())
     board[x-1][y-1].append((i+1, d-1))
 
-while cnt < 1000:
+cnt = 0
+isValid = True
+
+while cnt < 1000 and isValid:
     cnt += 1
-    temp = 0
 
     for x in range(K):
         i, j, k = findIndex(x+1)
         temp = move(i, j, k)
-        if temp >= 4:
-            break
 
-    if temp >= 4:
-        break
+        if temp >= 4:
+            isValid = False
+            break
 
 print(-1 if cnt == 1000 else cnt)
